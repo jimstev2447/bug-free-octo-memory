5ae3cac24c9e7a6379689c314dcf11b7f1000fc8
nc-hev-mag <150343854+nc-hev-mag@users.noreply.github.com>
2024-12-05 09:33:06 +0000

    add loop to log uppercase chars in tutor, else index

--- example.js
+++ example.js
@@ -5,3 +5,12 @@ console.log(newVariable);
 // adding a new line
 
 const tutor = "Kev";
+
+for (let i = 0; i < tutor.length; i++) {
+	const char = tutor[i];
+	if (char === char.toUpperCase()) {
+		console.log(char);
+	} else {
+		console.log(i);
+	}
+}
